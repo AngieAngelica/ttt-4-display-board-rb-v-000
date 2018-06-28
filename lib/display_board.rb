b4f6edfe079ced0dc6a346205975fe8a3f731a17
Angie Kassab <anasab12@gmail.com>
2018-06-28 01:58:02 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,17 @@
 def display_board(board)
+<<<<<<< HEAD
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+=======
+ 
+ puts " #{board[0]} | #{board[1]} | #{board[2]} "
+ puts "------------"
+ puts  " #{board[3]} | #{board[4]} | #{board[5]} "
+ puts "------------"
+ puts " #{board[6]} | #{board[7]} | #{board[8]} "
+ end
+>>>>>>> 4fc999ff205da7f1c9f6507a3e2f6751041f0553
